90d0b23f4d736df2fde07e1975838bde116a1c41
ikirpich <ikirpichny@mail.ru>
2019-10-07 13:54:16 +0300

    Lab2

---
+++ src/HomeWork/Lab2_9.java
@@ -0,0 +1,32 @@
+package HomeWork;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Lab2_9 {
+    String[] a1;
+    String[] a2;
+
+    public Lab2_9() {
+        this.a1 = a1;
+        this.a2 = a2;
+    }
+
+    String[] createName (String[] a1, String[] a2) {
+        int i = new Random().nextInt(a1.length);
+        int j = new Random().nextInt(a2.length);
+        String[] a3 = new String[] {a1[i], a2[j]};
+        System.out.println(Arrays.toString(a3));
+        return a3;
+    }
+
+    public static void main(String[] args) {
+        Lab2_9 lab2_9 = new Lab2_9();
+        String[] a1 = new String[] {"РАДИОАКТИВНЫЙ", "ГРЕЧНЕВЫЙ", "ДЕМОНИЧЕСКИЙ",  "ПРИЗРАЧНЫЙ",
+                "ОЗОРНОЙ", "ПРИЗРАЧНЫЙ", "КОСМИЧЕСКИЙ", "ЗВЕЗДНЫЙ", "СЕКСУАЛЬНЫЙ", "НЕПОБЕДИМЫЙ" };
+        String[] a2 = new String[] {"КАПИТАН", "ЭЛЬФ", "ПИНГВИН", "ИНДЕЕЦ", "ГНОМ", "УПЫРЬ", "БОРОДАЧ",
+                "БОБЕР", "КОРОЛЬ", "ТОЛСТОПУЗ", "КИЛЛЕР"};
+
+        lab2_9.createName(a1, a2);
+    }
+}
