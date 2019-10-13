b6c0da7bcb2183b1ca64339dea3fadc195444ab4
ikirpich <ikirpichny@mail.ru>
2019-10-13 22:11:15 +0300

    Class work

---
+++ src/ClassWork/Arrs.java
@@ -0,0 +1,33 @@
+package ClassWork;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Arrs {
+    Random random = new Random();
+    int[] arr = new int[random.nextInt(15)];
+
+    int[] capArr(){
+        int count = 0;
+        for(int i: arr){
+            arr[count] = random.nextInt(99);
+            count++;
+        }
+        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+
+    void minValArr(int[] arr){
+        int min = arr[0];
+        for(int i: arr){
+            if (i < min) min = i;
+        }
+        System.out.println("min: " + min);
+    }
+
+    public static void main(String[] args) {
+        Arrs arrs = new Arrs();
+        //arrs.capArr();
+//        arrs.minValArr(arr);
+    }
+}
