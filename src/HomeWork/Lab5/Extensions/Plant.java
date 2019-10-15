2d6ee48a73932e348334d39db161917c83ceae54
ikirpich <ikirpichny@mail.ru>
2019-10-15 13:04:56 +0300

    Lab5.Extension

--- src/HomeWork/Lab5/Extensions/Plant.java
+++ src/HomeWork/Lab5/Extensions/Plant.java
@@ -1,7 +1,7 @@
 package HomeWork.Lab5.Extensions;
 
 public class Plant extends Entity {
-    public Plant(boolean isAnimal, boolean isExist) {
+    public Plant(boolean isAnimal) {
         super(isAnimal);
     }
 }
