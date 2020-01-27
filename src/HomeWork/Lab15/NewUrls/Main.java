9d7249aff5016d2a95aebfa71af0a86152c8b74e
ikirpichny <ikirpichny@mail.ru>
2020-01-27 21:45:32 +0300

    main lab15

--- src/HomeWork/Lab15/NewUrls/Main.java
+++ src/HomeWork/Lab15/NewUrls/Main.java
@@ -7,5 +7,17 @@ public class Main {
 
         String url1 = new UrlBuilder("yandex.ru").setScheme("http").setResource("main").build();
         System.out.println(url1);
+
+        UrlBuilder url2 = new UrlBuilder("vk.com");
+//        System.out.println(url2.url);
+        System.out.println(url2.hashCode());
+        url2.setScheme("https");
+        System.out.println(url2.hashCode());
+        url2.setResource("feed");
+        System.out.println(url2.hashCode());
+//        System.out.println(url2.url);
+        System.out.println(url2.build());
+
+
     }
 }
